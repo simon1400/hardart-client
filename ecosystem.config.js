19e780cc02641f845a977e9b4715269a52e7b2f5
Your Name <>
2024-08-21 14:06:16 +0200

    setting deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,8 @@ module.exports = {
       user : 'dimi',
       host : ['89.221.216.23'],
       ref  : 'origin/main',
-      repo : 'git@github.com:simon1400/hardart-web.git',
-      path : '/home/dimi/app/hardart',
+      repo : 'git@github.com:simon1400/hardart-client.git',
+      path : '/home/dimi/app/hardart/client',
       'post-deploy' : 'npm i && npm run build && pm2 reload ecosystem.config.js --env production',
     }
   }
