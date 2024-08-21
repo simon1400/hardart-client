6be68a562112bd5f482b6e237f8b58e6b28f2a02
Your Name <>
2024-08-21 14:05:24 +0200

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [{
+    name   : "Hardart client",
+    script : "npm start"
+  }],
+
+  deploy : {
+    production : {
+      user : 'dimi',
+      host : ['89.221.216.23'],
+      ref  : 'origin/main',
+      repo : 'git@github.com:simon1400/hardart-web.git',
+      path : '/home/dimi/app/hardart',
+      'post-deploy' : 'npm i && npm run build && pm2 reload ecosystem.config.js --env production',
+    }
+  }
+};
